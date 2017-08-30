5121543c184ba016088d3c0c16f549e79e64caef
ZiyueHuang <zyhuang94@gmail.com>
2017-08-30 15:06:20 +0800

    LR Worker

---
+++ include/lrworker.cc
@@ -0,0 +1,43 @@
+#ifndef LRWorker_H
+#define LRWorker_H
+
+
+typedef float real_t;
+
+
+class LRWorker {
+public:
+  LRWorker(size_t num_dim, real_t learning_rate=0.001): num_dim_(num_dim), learning_rate_(learning_rate) {
+    kv = new KVWorker<float>(0);
+  }
+
+  ~LRWorker() {
+    if (kv) {
+      delete kv;
+    }
+  }
+
+  void Train(RowBlock& batch);
+
+  std::vector<int> Test(RowBlock& batch);
+
+  std::vector<real_t> GetWeight();
+
+  void InitWeight();
+
+  int Predict(std::vector<real_t>& features);
+
+  void PullWeight();
+
+  void PushGradient(std::vector<real_t>& grads);
+
+  size_t num_dim_;
+  real_t learning_rate_;
+
+  std::vector<real_t> weight_;
+
+  ps::KVWorker<real_t>* kv;
+};
+
+
+#endif  // LRWorker_H
